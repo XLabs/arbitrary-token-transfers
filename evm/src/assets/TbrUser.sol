cfa292ee41584fce6c8dfefcaf4cb7306b402d2b
Sebastián Claudio Nale <sebinale@gmail.com>
2024-09-11 00:26:11 -0300

    evm: adds first draft implementation of `CompleteTransfer`
    Tests and build are broken though 😢

--- evm/src/assets/TbrUser.sol
+++ evm/src/assets/TbrUser.sol
@@ -3,9 +3,11 @@
 pragma solidity ^0.8.25;
 
 import {BytesParsing} from "wormhole-sdk/libraries/BytesParsing.sol";
+import {fromUniversalAddress, forwardError} from "wormhole-sdk/Utils.sol";
+import {IWETH} from "wormhole-sdk/interfaces/token/IWETH.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {TRANSFER_TOKEN_WITH_RELAY_ID} from "./TbrIds.sol";
+import {TRANSFER_TOKEN_WITH_RELAY_ID, COMPLETE_TRANSFER_ID, TB_TRANSFER_WITH_PAYLOAD} from "./TbrIds.sol";
 import {TbrBase, InvalidCommand} from "./TbrBase.sol";
 
 uint8 constant ACQUIRE_PREAPPROVED = 0;
@@ -58,12 +60,34 @@ error FeesInsufficient(uint256 feeReceived, uint256 commandIndex);
  * The acquire mode is not implemented at this time.
  */
 error AcquireModeNotImplemented(uint8 acquireMode);
+/**
+ * The incoming TB message was sent from an unknown contract.
+ */
+error UnrecognizedPeer(uint16 sourceChain, bytes32 sourceAddress, uint256 commandIndex);
+/**
+ * The complete transfer command was supplied with insufficient gas tokens.
+ */
+error InsufficientGasDropoff(uint256 commandIndex);
+/**
+ * The token transfer cannot be completed because the token is not attested on this chain yet.
+ */
+error TokenNotAttested(bytes32 canonicalToken, uint16 canonicalChain);
+/**
+ * The gas token in this chain has no supported ERC20 tokenization.
+ */
+error GasTokenNotSupported();
+/**
+ * The gas token is only accepted through ERC20 tokenization withdrawals.
+ */
+error GasTokenOnlyAcceptedViaWithdrawal();
+/**
+ * The VAA does not contain a Token Bridge "Transfer with payload" message.
+ */
+error UnexpectedMessage(uint8 payloadId, uint256 commandIndex);
 
 event TransferRequested(address sender, uint64 sequence, uint32 gasDropoff, uint256 fee);
 
 abstract contract TbrUser is TbrBase {
-  using BytesParsing for bytes;
-
   function transferTokenWithRelay(bytes calldata data, uint256 unallocatedBalance, uint256 commandIndex) internal returns (uint256 fee, uint256 offset) {
     // The bounds of the command are determined by the `acquireMode` field.
     // So we take the opportunity to keep it from that check.
@@ -141,6 +165,71 @@ abstract contract TbrUser is TbrBase {
       unwrapIntent
     );
     return result;
+  }
+
+  /**
+   * Redeems a TB transfer VAA.
+   * Gas dropoff is reported to dispatcher so that any excedent can be refunded to the sender. 
+   */
+  function completeTransfer(bytes calldata data, uint256 unallocatedBalance, uint256 commandIndex) internal returns (uint256 gasDropoffSpent, uint256 offset) {
+    (bytes memory vaa, uint256 size, uint256 bodyOffset) = CompleteTransfer.extractVaa(data, commandIndex);
+    offset = size;
+
+    // Check that this is a known peer
+    (bytes32 sourceAddress, uint16 sourceChain) = CompleteTransfer.decodeSource(vaa, bodyOffset);
+    if (!isPeer(sourceChain, sourceAddress)) {
+      revert UnrecognizedPeer(sourceChain, sourceAddress, commandIndex);
+    }
+
+    // TODO: evaluate whether to define the token amount received based on balance difference before and after transferring tokens from the TB.
+    // Some tokens might implement distributed supply expansion / contraction. Doing the above would let us support those.
+
+    // Perform redeem in TB
+    // TODO: ignore return value? The gas savings might be very low.
+    tokenBridge.completeTransferWithPayload(vaa);
+
+    (address destination, uint256 tokenAmount, uint256 gasDropoff, bool unwrapIntent) = CompleteTransfer.decodeUserParams(vaa, bodyOffset);
+
+    if (gasDropoff > unallocatedBalance) {
+      revert InsufficientGasDropoff(commandIndex);
+    }
+
+    // If an unwrap is desired, unwrap and call destination with full amount
+    if (unwrapIntent && gasErc20TokenizationIsExplicit) {
+      gasToken.withdraw(tokenAmount);
+
+      // Transfer full amount
+      (bool success, bytes memory result) = destination.call{value: gasDropoff + tokenAmount}("");
+      if (!success) forwardError(result);
+      return (gasDropoff, offset);
+    }
+
+    IERC20 token = deriveToken(vaa, bodyOffset);
+    // Otherwise, transfer tokens and perform gas dropoff
+    SafeERC20.safeTransfer(token, destination, tokenAmount);
+    // Transfer gas dropoff
+    (bool success, bytes memory result) = destination.call{value: gasDropoff}("");
+    if (!success) forwardError(result);
+  }
+
+  function deriveToken(bytes memory vaa, uint256 bodyOffset) view internal returns (IERC20 token) {
+    (bytes32 canonicalToken, uint16 canonicalChain) = CompleteTransfer.decodeToken(vaa, bodyOffset);
+    // TODO: compute the derived address here to avoid doing a call to the token bridge.
+    // The gas savings shouldn't be that big so it's low priority.
+    token = IERC20(tokenBridge.wrappedAsset(canonicalChain, canonicalToken));
+    if (address(token) == address(0)) revert TokenNotAttested(canonicalToken, canonicalChain);
+  }
+
+
+  receive() external payable {
+    // NOTE: all values retrieved here need to be immutables to avoid going over the 2300 gas stipend
+    if (!gasErc20TokenizationIsExplicit) {
+      revert GasTokenNotSupported();
+    }
+
+    if (msg.sender != address(gasToken)) {
+      revert GasTokenOnlyAcceptedViaWithdrawal();
+    }
   }
 }
 
@@ -232,4 +321,137 @@ library TransferTokenWithRelay {
     return unwrapIntent;
   }
 
+}
+
+library CompleteTransfer {
+  using BytesParsing for bytes;
+  // The complete transfer command just has a single parameter: a VAA.
+  // So here we have constants and code that helps parse the relevant parts of a VAA.
+  //   2 bytes uint vaa length
+  uint256 constant private VAA_LENGTH_OFFSET = 0;
+  uint256 constant private VAA_OFFSET = VAA_LENGTH_OFFSET + 2;
+
+  // start of VAA header
+
+  //   1 byte uint vaa version, should be 0
+  uint256 constant private VAA_VERSION_OFFSET = 0;
+  //   4 bytes uint guardian set
+  uint256 constant private GUARDIAN_SET_OFFSET = VAA_VERSION_OFFSET + 1;
+  //   1 byte signature array length
+  uint256 constant private SIGNATURE_ARRAY_LENGTH_OFFSET = GUARDIAN_SET_OFFSET + 4;
+
+  uint256 constant private FIRST_SIGNATURE_OFFSET = SIGNATURE_ARRAY_LENGTH_OFFSET + 1;
+  // Layout of each signature:
+  //   1 byte uint guardian index
+  uint256 constant private GUARDIAN_INDEX_OFFSET = 0;
+  //   65 bytes signature
+  uint256 constant private SIGNATURE_OFFSET = GUARDIAN_INDEX_OFFSET + 1;
+  uint256 constant SIGNATURE_SIZE = 66;
+
+  // end of VAA header
+  // start of VAA body
+
+  //   4 bytes uint timestamp
+  uint256 constant private TIMESTAMP_OFFSET = 0;
+  //   4 bytes uint nonce
+  uint256 constant private NONCE_OFFSET = TIMESTAMP_OFFSET + 4;
+  //   2 bytes uint source chain id
+  uint256 constant private SOURCE_CHAIN_OFFSET = NONCE_OFFSET + 4;
+  //   32 bytes emitter address
+  uint256 constant private EMITTER_ADDRESS_OFFSET = SOURCE_CHAIN_OFFSET + 2;
+  //   8 bytes uint sequence
+  uint256 constant private SEQUENCE_OFFSET = EMITTER_ADDRESS_OFFSET + 32;
+  //   1 byte uint consistency level
+  uint256 constant private CONSISTENCY_LEVEL_OFFSET = SEQUENCE_OFFSET + 8;
+
+  // Start of custom VAA payload
+  // See https://github.com/wormhole-foundation/wormhole-sdk-ts/blob/main/core/definitions/src/protocols/tokenBridge/tokenBridgeLayout.ts#L49
+  //   1 byte uint payload id
+  //     - 1, "Transfer" -> reject
+  //     - 2, "AttestMeta" -> reject
+  //     - 3, "TransferWithPayload"
+  uint256 constant private PAYLOAD_ID_OFFSET = CONSISTENCY_LEVEL_OFFSET + 1;
+  // These correspond to "TransferWithPayload" layout
+  //   32 bytes uint amount
+  uint256 constant private AMOUNT_OFFSET = PAYLOAD_ID_OFFSET + 1;
+  //   32 bytes token address
+  uint256 constant private TOKEN_ADDRESS_OFFSET = AMOUNT_OFFSET + 32;
+  //   2 bytes uint canonical token chain id
+  uint256 constant private TOKEN_CHAIN_OFFSET = TOKEN_ADDRESS_OFFSET + 32;
+  //   32 bytes TB destination address
+  uint256 constant private TB_DESTINATION_ADDRESS_OFFSET = TOKEN_CHAIN_OFFSET + 2;
+  //   2 bytes uint TB destination chain
+  uint256 constant private TB_DESTINATION_CHAIN_OFFSET = TB_DESTINATION_ADDRESS_OFFSET + 32;
+  //   32 bytes source address
+  uint256 constant private SOURCE_ADDRESS_OFFSET = TB_DESTINATION_CHAIN_OFFSET + 2;
+
+  // Start of nested TB custom payload
+  // Here goes the TBRv3 message
+  //   1 byte uint message version
+  uint256 constant private TBR_VERSION_OFFSET = SOURCE_ADDRESS_OFFSET + 32;
+  //   32 bytes user address that receives the tokens
+  uint256 constant private TOKEN_RECIPIENT_OFFSET = TBR_VERSION_OFFSET + 1;
+  //   4 bytes uint gas dropoff requested
+  uint256 constant private GAS_DROPOFF_OFFSET = TOKEN_RECIPIENT_OFFSET + 32;
+  //   1 byte boolean unwrap intent
+  uint256 constant private UNWRAP_INTENT_OFFSET = GAS_DROPOFF_OFFSET + 4;
+
+  // End of nested TB custom payload and VAA payload
+  uint256 constant private VAA_BODY_SIZE = UNWRAP_INTENT_OFFSET + 1;
+
+  /**
+   * The CompleteTransfer command has a single argument: the VAA.
+   * So we return the offset past the end of the command here too.
+   */
+  function extractVaa(bytes calldata data, uint256 commandIndex) pure internal returns(bytes memory, uint256, uint256) {
+    uint256 vaaLength = uint256(uint8(data[VAA_LENGTH_OFFSET]));
+
+    // We copy the whole VAA because we need to forward it to the token bridge.
+    bytes memory vaa = data[VAA_VERSION_OFFSET:vaaLength];
+
+    (uint256 signatures,) = vaa.asUint8Unchecked(SIGNATURE_ARRAY_LENGTH_OFFSET);
+    // uint256 signatures = uint256();
+    uint256 signatureArraySize = signatures * SIGNATURE_SIZE;
+    uint256 bodyOffset = FIRST_SIGNATURE_OFFSET + signatureArraySize;
+    uint256 expectedLength = bodyOffset + VAA_BODY_SIZE;
+    uint256 totalSize = VAA_OFFSET + vaaLength;
+    if (vaaLength != expectedLength || totalSize > data.length) {
+      revert InvalidCommand(COMPLETE_TRANSFER_ID, commandIndex);
+    }
+
+    // Checking that the emitter is the token bridge is possible at this point but isn't really worth it since this is something
+    // checked by the token bridge itself when processing the VAA.
+
+    (uint8 payloadId,) = vaa.asUint8Unchecked(bodyOffset + PAYLOAD_ID_OFFSET);
+    if (payloadId != TB_TRANSFER_WITH_PAYLOAD) {
+      revert UnexpectedMessage(payloadId, commandIndex);
+    }
+
+    return (vaa, totalSize, bodyOffset);
+  }
+
+  // These functions assume that the length check in `extractVaa` was performed
+
+  function decodeSource(bytes memory vaa, uint256 bodyOffset) pure internal returns(bytes32 sourceAddress, uint16 sourceChain) {
+    (sourceAddress,) = vaa.asBytes32Unchecked(bodyOffset + SOURCE_ADDRESS_OFFSET);
+    (sourceChain,) = vaa.asUint16Unchecked(bodyOffset + SOURCE_CHAIN_OFFSET);
+  }
+
+  function decodeToken(bytes memory vaa, uint256 bodyOffset) pure internal returns(bytes32 canonicalToken, uint16 canonicalChain) {
+    (canonicalToken,) = vaa.asBytes32Unchecked(bodyOffset + TOKEN_ADDRESS_OFFSET);
+    (canonicalChain,) = vaa.asUint16Unchecked(bodyOffset + TOKEN_CHAIN_OFFSET);
+  }
+
+  function decodeUserParams(bytes memory vaa, uint256 bodyOffset) pure internal returns(
+    address destination,
+    uint256 tokenAmount,
+    uint256 gasDropoff,
+    bool unwrapIntent
+  ) {
+    (bytes32 destinationUniversal,) = vaa.asBytes32Unchecked(bodyOffset + TOKEN_RECIPIENT_OFFSET);
+    destination = fromUniversalAddress(destinationUniversal);
+    (tokenAmount,) = vaa.asUint256Unchecked(bodyOffset + AMOUNT_OFFSET);
+    (gasDropoff,) = vaa.asUint32Unchecked(bodyOffset + GAS_DROPOFF_OFFSET);
+    (unwrapIntent,) = vaa.asBoolUnchecked(bodyOffset + UNWRAP_INTENT_OFFSET);
+  }
 }
