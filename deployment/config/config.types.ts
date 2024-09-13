96d5a613f9ce9747c7834f472157faaf483ad329
solanoe <solanoepalacio@gmail.com>
2024-09-13 11:30:01 -0300

    tbr-v3-config types

--- deployment/config/config.types.ts
+++ deployment/config/config.types.ts
@@ -1,12 +1,22 @@
 import { ChainConfig } from "../helpers";
 
-export type Tbrv3Config = ChainConfig & {
+export type BaseTbrV3Config = ChainConfig & {
   owner?: string;
   admin?: string;
   feeRecipient?: string;
+  relayFee: number;
+  maxGasDropoff: bigint;  
+}
+
+// TODO: in reality this should limit ChainConfig to only containing evm chain ids..
+export type EvmTbrV3Config = BaseTbrV3Config & {
   oracleVersion: 0;
   initGasErc20TokenizationIsExplicit: boolean;
-  relayFee: number;
-  maxGasDropoff: bigint;
   txSizeSensitive: boolean;
+};
+
+// TODO: in reality this should limit ChainConfig to only containing solana chain ids..
+export type SolanaTbrV3Config = BaseTbrV3Config & {
+  evmTransactionGas: bigint;
+  evmTransactionSize: bigint;
 };
