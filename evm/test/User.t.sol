9341c857ea07642d425af912c7253f26afd3baea
Agustin Velez <agustinmaurasvelez@gmail.com>
2024-09-13 12:34:39 -0300

    Add TransferTokenWithRelay border tests

--- evm/test/User.t.sol
+++ evm/test/User.t.sol
@@ -7,9 +7,9 @@ import {IPriceOracle} from "price-oracle/IPriceOracle.sol";
 import "wormhole-sdk/interfaces/ITokenBridge.sol";
 import "wormhole-sdk/libraries/BytesParsing.sol";
 
-import {ACQUIRE_PREAPPROVED} from "tbr/assets/TbrUser.sol";
 import {TransferRequested} from "tbr/assets/TbrUser.sol";
 import "tbr/assets/TbrDispatcher.sol";
+import "tbr/assets/TbrUser.sol";
 import "tbr/assets/TbrIds.sol";
 
 import {TbrTestBase} from "./utils/TbrTestBase.sol";
@@ -18,12 +18,12 @@ import {getFakeBytes32} from "./utils/utils.sol";
 contract UserTest is TbrTestBase {
   using BytesParsing for bytes;
   
-  uint16  SOLANA_CHAIN_ID       = 1;
-  bytes32 SOLANA_CANONICAL_PEER = getFakeBytes32("SOLANA_CANONICAL_PEER");
-  uint16  EVM_CHAIN_ID          = 3;
-  bytes32 EVM_CANONICAL_PEER    = getFakeBytes32("EVM_CANONICAL_PEER");
-  uint32 MAX_GAS_DROPOFF_AMOUNT = 10000;
-  uint32 RELAY_FEE_AMOUNT       = 1000;
+  uint16  SOLANA_CHAIN_ID        = 1;
+  bytes32 SOLANA_CANONICAL_PEER  = getFakeBytes32("SOLANA_CANONICAL_PEER");
+  uint16  EVM_CHAIN_ID           = 3;
+  bytes32 EVM_CANONICAL_PEER     = getFakeBytes32("EVM_CANONICAL_PEER");
+  uint32  MAX_GAS_DROPOFF_AMOUNT = 10000;
+  uint32  RELAY_FEE_AMOUNT       = 1000;
 
   receive() external payable {}
 
@@ -43,6 +43,14 @@ contract UserTest is TbrTestBase {
   }
 
   function _setUp1() internal override {
+    // Mock the bridge contract address on destination chain returned
+    bytes32 targetTokenBridge = getFakeBytes32("targetTokenBridge");
+    vm.mockCall(
+      address(tokenBridge), 
+      abi.encodeWithSelector(ITokenBridge.bridgeContracts.selector), 
+      abi.encode(targetTokenBridge)
+    );
+
     // Solana chain setup
     executeGovernanceCommand(
       abi.encodePacked(UPDATE_CANONICAL_PEER, SOLANA_CHAIN_ID, SOLANA_CANONICAL_PEER)
@@ -90,14 +98,6 @@ contract UserTest is TbrTestBase {
     SafeERC20.safeApprove(usdt, address(tbr), tokenAmount);
 
     /// Mocks
-
-    // Mock the bridge contract address on destination chain returned
-    bytes32 targetTokenBridge = getFakeBytes32("targetTokenBridge");
-    vm.mockCall(
-      address(tokenBridge), 
-      abi.encodeWithSelector(ITokenBridge.bridgeContracts.selector), 
-      abi.encode(targetTokenBridge)
-    );
 
     // Mock the sequence number returned by token bridge 
     uint64 sequence = 1;
@@ -164,6 +164,204 @@ contract UserTest is TbrTestBase {
 
     assertEq(address(this).balance, initialCallerBalance - feeQuote);
     assertEq(address(feeRecipient).balance, initialFeeRecipientBalance + feeQuote);
-    
+  }
+
+  function testTransferTokenWithRelay_TokenDoesNotExist () public {
+    bytes32 recipient = getFakeBytes32("recipient");
+    address fakeToken = address(0);
+    bool unwrapIntent = false;
+    uint256 tokenAmount = 1e6;
+    uint32 gasDropoff = 100;
+    uint msgValue = 1e6;
+
+    vm.expectRevert(
+      abi.encodeWithSelector(TokenDoesNotExist.selector, fakeToken)
+    );
+
+    invokeTbr(
+      abi.encodePacked(
+        tbr.exec768.selector, 
+        DISPATCHER_PROTOCOL_VERSION0, 
+        TRANSFER_TOKEN_WITH_RELAY_ID,
+        recipient,
+        SOLANA_CHAIN_ID,
+        fakeToken,
+        tokenAmount,
+        gasDropoff,
+        unwrapIntent,
+        ACQUIRE_PREAPPROVED
+      ),
+      msgValue
+    );
+  }
+
+  function testTransferTokenWithRelay_DestinationChainNotSupported (uint16 fakeChainId) public {
+    bytes32 recipient = getFakeBytes32("recipient");
+    bool unwrapIntent = false;
+    uint256 tokenAmount = 1e6;
+    uint32 gasDropoff = 100;
+    uint msgValue = 1e6;
+
+    vm.assume(fakeChainId != SOLANA_CHAIN_ID);
+    vm.assume(fakeChainId != EVM_CHAIN_ID);
+    vm.expectRevert(
+      abi.encodeWithSelector(DestinationChainIsNotSupported.selector, fakeChainId)
+    );
+
+    invokeTbr(
+      abi.encodePacked(
+        tbr.exec768.selector, 
+        DISPATCHER_PROTOCOL_VERSION0, 
+        TRANSFER_TOKEN_WITH_RELAY_ID,
+        recipient,
+        fakeChainId,
+        address(usdt),
+        tokenAmount,
+        gasDropoff,
+        unwrapIntent,
+        ACQUIRE_PREAPPROVED
+      ),
+      msgValue
+    );
+  }
+
+  function testTransferTokenWithRelay_TransfersToChainArePaused () public {
+    bytes32 recipient = getFakeBytes32("recipient");
+    bool unwrapIntent = false;
+    uint256 tokenAmount = 1e6;
+    uint32 gasDropoff = 100;
+    uint msgValue = 1e6;
+
+    executeGovernanceCommand(
+      abi.encodePacked(PAUSE_CHAIN, SOLANA_CHAIN_ID, bool(true))
+    );
+
+    vm.expectRevert(
+      abi.encodeWithSelector(TransfersToChainArePaused.selector, SOLANA_CHAIN_ID)
+    );
+
+    invokeTbr(
+      abi.encodePacked(
+        tbr.exec768.selector, 
+        DISPATCHER_PROTOCOL_VERSION0, 
+        TRANSFER_TOKEN_WITH_RELAY_ID,
+        recipient,
+        SOLANA_CHAIN_ID,
+        address(usdt),
+        tokenAmount,
+        gasDropoff,
+        unwrapIntent,
+        ACQUIRE_PREAPPROVED
+      ),
+      msgValue
+    );
+  }
+
+  function testTransferTokenWithRelay_GasDropoffExceedsMaximum () public {
+    bytes32 recipient = getFakeBytes32("recipient");
+    uint32 gasDropoff = MAX_GAS_DROPOFF_AMOUNT + 1;
+    bool unwrapIntent = false;
+    uint256 tokenAmount = 1e6;
+    uint256 commandIndex = 0;
+    uint msgValue = 1e6;
+
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        GasDropoffRequestedExceedsMaximum.selector, 
+        MAX_GAS_DROPOFF_AMOUNT, 
+        commandIndex
+      )
+    );
+
+    invokeTbr(
+      abi.encodePacked(
+        tbr.exec768.selector, 
+        DISPATCHER_PROTOCOL_VERSION0, 
+        TRANSFER_TOKEN_WITH_RELAY_ID,
+        recipient,
+        SOLANA_CHAIN_ID,
+        address(usdt),
+        tokenAmount,
+        gasDropoff,
+        unwrapIntent,
+        ACQUIRE_PREAPPROVED
+      ),
+      msgValue
+    );
+  }
+
+  function testTransferTokenWithRelay_FeesInsufficient () public {
+    bytes32 recipient = getFakeBytes32("recipient");
+    uint32 gasDropoff = 1000;
+    bool unwrapIntent = false;
+    uint256 tokenAmount = 1e6;
+    uint256 commandIndex = 0;
+    uint msgValue = 10;
+
+    // Mock the quote returned by the oracle
+    uint feeQuote = 100;
+    vm.mockCall(
+      address(oracle), 
+      abi.encodeWithSelector(IPriceOracle.get1959.selector), 
+      abi.encode(abi.encodePacked(uint256(feeQuote)))
+    );
+
+    vm.expectRevert(
+      abi.encodeWithSelector(FeesInsufficient.selector, msgValue, commandIndex)
+    );
+
+    invokeTbr(
+      abi.encodePacked(
+        tbr.exec768.selector, 
+        DISPATCHER_PROTOCOL_VERSION0, 
+        TRANSFER_TOKEN_WITH_RELAY_ID,
+        recipient,
+        SOLANA_CHAIN_ID,
+        address(usdt),
+        tokenAmount,
+        gasDropoff,
+        unwrapIntent,
+        ACQUIRE_PREAPPROVED
+      ),
+      msgValue
+    );
+  }
+
+  function testTransferTokenWithRelay_AcquireModeNotImplemented (uint8 acquireMode) public {
+    bytes32 recipient = getFakeBytes32("recipient");
+    uint32 gasDropoff = 1000;
+    bool unwrapIntent = false;
+    uint256 tokenAmount = 1e6;
+    uint msgValue = 1e6;
+
+    // Mock the quote returned by the oracle
+    uint feeQuote = 100;
+    vm.mockCall(
+      address(oracle), 
+      abi.encodeWithSelector(IPriceOracle.get1959.selector), 
+      abi.encode(abi.encodePacked(uint256(feeQuote)))
+    );
+
+    // TODO: add the rest of the acquire modes when implemented
+    vm.assume(acquireMode != ACQUIRE_PREAPPROVED);
+    vm.expectRevert(
+      abi.encodeWithSelector(AcquireModeNotImplemented.selector, acquireMode)
+    );
+
+    invokeTbr(
+      abi.encodePacked(
+        tbr.exec768.selector, 
+        DISPATCHER_PROTOCOL_VERSION0, 
+        TRANSFER_TOKEN_WITH_RELAY_ID,
+        recipient,
+        SOLANA_CHAIN_ID,
+        address(usdt),
+        tokenAmount,
+        gasDropoff,
+        unwrapIntent,
+        acquireMode
+      ),
+      msgValue
+    );
   }
 }
