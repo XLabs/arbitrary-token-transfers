9faae3f7b54294ac07957cb5f131b05aaf5552b4
Félix <felix@xlabs.xyz>
2024-09-18 21:51:58 +0200

    Fix recipient parsing

--- sdk/solana/tbrv3/index.ts
+++ sdk/solana/tbrv3/index.ts
@@ -19,12 +19,45 @@ import { SolanaPriceOracleClient } from '@xlabs/solana-price-oracle-sdk';
 
 import { TokenBridgeRelayer } from './idl/token_bridge_relayer.js';
 import IDL from '../../../target/idl/token_bridge_relayer.json' with { type: 'json' };
-import { inspect } from 'util';
 
 // Export IDL
 export * from './idl/token_bridge_relayer.js';
 export const idl = IDL;
 export { SolanaPriceOracleClient } from '@xlabs/solana-price-oracle-sdk';
+
+//================== Should be in a package
+import { CustomConversion, Layout, layout, LayoutItem } from '@wormhole-foundation/sdk-base';
+import { layoutItems } from '@wormhole-foundation/sdk-definitions';
+
+//TODO eliminate copy paste from oracle sdk and unify in some shared repo
+const decimalDownShift = (downShift: number) =>
+  ({
+    to: (val: number): number => val / 10 ** downShift,
+    from: (price: number): number => {
+      const encoded = Math.round(price * 10 ** downShift);
+      if (encoded === 0 && price !== 0)
+        throw new Error(`losing all precision when storing ${price} with shift ${downShift}`);
+
+      return encoded;
+    },
+  }) as const satisfies CustomConversion<number, number>;
+
+//specifed as: gas token (i.e. eth, avax, ...)
+// encoded as: µgas token
+const gasDropoffItem = {
+  binary: 'uint',
+  size: 4,
+  custom: decimalDownShift(6),
+} as const satisfies LayoutItem;
+
+const TBRv3Message = [
+  //we can turn this into a switch layout if we ever get a version 1
+  { name: 'version', binary: 'uint', size: 1, custom: 0, omit: true },
+  { name: 'recipient', ...layoutItems.universalAddressItem },
+  { name: 'gasDropoff', ...gasDropoffItem },
+  { name: 'unwrapIntent', ...layoutItems.boolItem },
+] as const satisfies Layout;
+//==================
 
 /**
  * 32 bytes.
@@ -437,13 +470,13 @@ export class SolanaTokenBridgeRelayer {
     signer: PublicKey,
     vaa: VaaMessage,
     recipientTokenAccount: PublicKey,
-    recipient: PublicKey,
   ): Promise<web3.TransactionInstruction> {
     const tokenBridgeAccounts = completeNativeTokenBridgeAccounts({
       tokenBridgeProgramId: this.tokenBridgeProgramId,
       wormholeProgramId: this.wormholeProgramId,
       vaa,
     });
+    const { recipient } = layout.deserializeLayout(TBRv3Message, vaa.payload.payload);
 
     return this.program.methods
       .completeTransfer(Array.from(vaa.hash))
@@ -451,7 +484,7 @@ export class SolanaTokenBridgeRelayer {
         payer: signer,
         tbrConfig: this.address.config(),
         recipientTokenAccount,
-        recipient,
+        recipient: new PublicKey(recipient.address),
         vaa: pda.vaa(this.wormholeProgramId, vaa.hash),
         temporaryAccount: pda.temporary(this.program.programId, tokenBridgeAccounts.mint),
         ...tokenBridgeAccounts,
@@ -466,13 +499,13 @@ export class SolanaTokenBridgeRelayer {
     signer: PublicKey,
     vaa: VaaMessage,
     recipientTokenAccount: PublicKey,
-    recipient: PublicKey,
   ): Promise<web3.TransactionInstruction> {
     const tokenBridgeAccounts = completeWrappedTokenBridgeAccounts({
       tokenBridgeProgramId: this.tokenBridgeProgramId,
       wormholeProgramId: this.wormholeProgramId,
       vaa,
     });
+    const { recipient } = layout.deserializeLayout(TBRv3Message, vaa.payload.payload);
 
     return this.program.methods
       .completeTransfer(Array.from(vaa.hash))
@@ -480,7 +513,7 @@ export class SolanaTokenBridgeRelayer {
         payer: signer,
         tbrConfig: this.address.config(),
         recipientTokenAccount,
-        recipient,
+        recipient: new PublicKey(recipient.address),
         vaa: pda.vaa(this.wormholeProgramId, vaa.hash),
         temporaryAccount: pda.temporary(this.program.programId, tokenBridgeAccounts.mint),
         ...tokenBridgeAccounts,
