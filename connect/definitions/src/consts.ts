eecfe18683fd1acedfaaa3c4f100194ea2e6667c
M-Picco <martin@xlabs.xyz>
2024-10-02 14:26:50 -0300

    connect: fix sepolia address

--- connect/definitions/src/consts.ts
+++ connect/definitions/src/consts.ts
@@ -5,7 +5,7 @@ const addresses = [[
   "Mainnet", []], [
   "Testnet", [
     ["Solana",          "ATT7enfWTMV4dDTe2NQ2yBaTByuXXwrYRznsioQEUW6r"],
-    ["Sepolia",         "0xb620f38B1211bDB96684576017F66bA9871eb6be"],
+    ["Sepolia",         "0x77A70bCA55813ec70528184F7Cb02eFeD71D6031"],
     ["Celo",            "0xdbe75EfF77dF67524B13c38e07d2451456098DC7"],
     ["BaseSepolia",     "0xFe8dE1cf8893f0D928F007E787fD072660EAc06B"],
     ["OptimismSepolia", "0x7057447A58b92e2C68A46548B6992203233e92eC"],
