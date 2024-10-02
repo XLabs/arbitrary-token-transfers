00f48d22a8b4dd21ab378837ca5eb2dd681a71bc
Agustin Velez <agustinmaurasvelez@gmail.com>
2024-10-02 11:15:25 -0300

    evm: refactor access control role checking

--- evm/src/assets/sharedComponents/SweepTokens.sol
+++ evm/src/assets/sharedComponents/SweepTokens.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.25;
 
 import { BytesParsing } from "wormhole-sdk/libraries/BytesParsing.sol";
-import { AccessControlState, accessControlState, NotAuthorized } from "./AccessControl.sol";
+import { senderHasAuth } from "./AccessControl.sol";
 import { transferTokens } from "./utils.sol";
 import "./ids.sol";
 
@@ -14,9 +14,7 @@ abstract contract SweepTokens {
     bytes calldata commands,
     uint offset
   ) internal returns (uint) {
-    AccessControlState storage state = accessControlState();
-    if (msg.sender != state.owner && !state.isAdmin[msg.sender]) 
-      revert NotAuthorized();
+    senderHasAuth();
 
     address token;
     uint256 amount;
