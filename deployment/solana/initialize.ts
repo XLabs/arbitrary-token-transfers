17c015b91a9b9f2af8773bf810384476c35fa903
hernandp <hernan.di.pietro@gmail.com>
2024-10-04 14:51:13 -0300

    Enforce JSON fields check

--- deployment/solana/initialize.ts
+++ deployment/solana/initialize.ts
@@ -4,6 +4,8 @@ import { SolanaChainInfo, LoggerFn } from '../helpers/interfaces.js';
 import { PublicKey } from '@solana/web3.js';
 import { loadSolanaTbrInitParams } from '../helpers/env.js';
 
+const SOLANA_DEFAULT_ADDRESS = '11111111111111111111111111111111';
+
 runOnSolana('initialize-tbr', initializeSolanaTbr).catch((e) => {
   console.error('Error executing script: ', e);
 });
@@ -18,6 +20,18 @@ async function initializeSolanaTbr(
   const tbr = new SolanaTokenBridgeRelayer({ connection });
 
   const tbrInitParams = loadSolanaTbrInitParams();
+
+  if (!tbrInitParams.owner || tbrInitParams.owner === SOLANA_DEFAULT_ADDRESS) {
+    throw new Error ("initialization parameters: owner address absent or not configured.")
+  }
+
+  if (!tbrInitParams.feeRecipient || tbrInitParams.feeRecipient === SOLANA_DEFAULT_ADDRESS) {
+    throw new Error ("initialization parameters: feeRecipient address absent or not configured.")
+  }
+
+  if (!tbrInitParams.admins || tbrInitParams.admins.length === 0 || tbrInitParams.admins.includes(SOLANA_DEFAULT_ADDRESS)) {
+    throw new Error ("initialization parameters: admins addresses absent or not configured.")
+  }
 
   const initializeIx = await tbr.initialize({
     owner: new PublicKey(tbrInitParams.owner),
