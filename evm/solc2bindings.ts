ff0b9acc54ddb933c453da5f071984c65ebb17d6
hernandp <hernan.di.pietro@gmail.com>
2024-12-04 12:39:03 -0300

    solc2bindings script

---
+++ evm/solc2bindings.ts
@@ -0,0 +1,39 @@
+import { runTypeChainInMemory, PublicConfig, FileDescription } from '@xlabs-xyz/typechain';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { exit } from 'process';
+
+(async () => {
+
+    if (process.argv.length < 4) {
+        console.log('Generate Typechain bindings from Solc output')
+        console.log(`\nsolc2bindings <inputFile> <outDir>`)
+        exit(1)
+    }
+
+    const outDir = process.argv[3];
+    const inputFile = process.argv[2];
+
+    const data = await readFile(inputFile, null)
+    const json = JSON.parse(data.toString())
+    const contracts: any = json['contracts']
+    const fileDescriptions: FileDescription[] = []
+
+    console.log(`Found ${Object.keys(contracts).length} contracts to process...`)
+
+    for (const [path, contents] of Object.entries(contracts)) {
+        fileDescriptions.push({ path, contents: JSON.stringify(contents) });
+    }
+    const pc: Omit<PublicConfig, "filesToProcess"> = {
+        cwd: path.join(process.cwd()),
+        allFiles: fileDescriptions.map(k => k.contents),
+        target: 'ethers-v6',
+        outDir
+    }
+
+    const result = await runTypeChainInMemory(pc, fileDescriptions)
+
+    console.log(`Generated ${result.filesGenerated} file(s)`)
+})()
+
+
