0447161a442d7bbfbb68de7e9be35a37dbdf6085
Sebastián Claudio Nale <sebinale@gmail.com>
2024-12-19 15:00:49 -0300

    connect: reorder address definitions

--- connect/definitions/src/consts.ts
+++ connect/definitions/src/consts.ts
@@ -6,8 +6,8 @@ const addresses = [[
   "Testnet", [
     ["Solana",          "ATtNvUvPZ3RU78P8Z5NuwHMTwQ8u8YsDWJ6YN6XvCErS"],
     ["Avalanche",       "0x2BCC362643B0aa3b2608de68b2C2ef2e6eFd2bb6"],
-    ["Sepolia",         "0xDc74c34F88d17895e31792439eaCE7cB1ed17d3a"],
     ["Celo",            "0xAafD9ED1B11b1E1Bf08094Fa0E53e4eEa807B5D5"],
+    ["Sepolia",         "0xDc74c34F88d17895e31792439eaCE7cB1ed17d3a"],
     ["ArbitrumSepolia", "0x7Ab71581b33948DdD07a4995a594d631BC4D2988"],
     ["BaseSepolia",     "0xFe8dE1cf8893f0D928F007E787fD072660EAc06B"],
     ["OptimismSepolia", "0x7057447A58b92e2C68A46548B6992203233e92eC"],
