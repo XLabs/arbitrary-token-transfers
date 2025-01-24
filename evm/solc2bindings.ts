7ec5a95b3fcd846561780bd6f99b5171e6a0c254
Sebastián Claudio Nale <sebinale@gmail.com>
2025-01-24 17:28:55 -0300

    deployment: fixes solc2bindings script

--- evm/solc2bindings.ts
+++ evm/solc2bindings.ts
@@ -1,45 +1,44 @@
-import { runTypeChainInMemory, PublicConfig, FileDescription } from '@xlabs-xyz/typechain';
+import { runTypeChainInMemory, FileDescription } from '@xlabs-xyz/typechain';
+import ethersv6Codegen from "@xlabs-xyz/typechain-ethers-v6";
 import { readFile } from 'fs/promises';
 import path from 'path';
-import { exit } from 'process';
 
 (async () => {
 
     if (process.argv.length < 4) {
-        console.log('Generate Typechain bindings from Solc output')
-        console.log(`\nsolc2bindings <inputFile> <outDir>`)
-        exit(1)
+        console.log('Generate Typechain bindings from Solc output');
+        console.log(`\nsolc2bindings <inputFile> <outDir>`);
+        process.exit(1);
     }
 
     const outDir = process.argv[3];
     const inputFile = process.argv[2];
 
-    const data = await readFile(inputFile, null)
-    const json = JSON.parse(data.toString())
-    const contracts: any = json['contracts']
-    const fileDescriptions: FileDescription[] = []
+    const data = await readFile(inputFile, null);
+    const json = JSON.parse(data.toString());
+    const contracts: any = json['contracts'];
+    const fileDescriptions: FileDescription[] = [];
 
-    console.log(`Found ${Object.keys(contracts).length} entries to process...`)
-    
+    console.log(`Found ${Object.keys(contracts).length} entries to process...`);
+
     for (const [path, contents] of Object.entries(contracts)) {
         const contractsInFile = Object.entries(contents as { [key: string]: any });
-        console.log(`  Found path ${path} with ${contractsInFile.length} contracts `)
+        console.log(`  Found path ${path} with ${contractsInFile.length} contracts `);
         for (const [contractName, abiAndBytecodeData] of contractsInFile) {
-            console.log(`    Adding contract ${contractName} `)
+            console.log(`    Adding contract ${contractName} `);
             fileDescriptions.push({ path, contents: JSON.stringify(abiAndBytecodeData) });
         }
     }
-    
-    const pc: Omit<PublicConfig, "filesToProcess"> = {
+
+    const pc = {
         cwd: path.join(process.cwd()),
         allFiles: fileDescriptions.map(k => k.contents),
-        target: 'ethers-v6',
         outDir
-    }
+    };
 
-    const result = await runTypeChainInMemory(pc, fileDescriptions)
+    const result = await runTypeChainInMemory(pc, fileDescriptions, ethersv6Codegen);
 
-    console.log(`Generated ${result.filesGenerated} file(s)`)
+    console.log(`Generated ${result.filesGenerated} file(s)`);
 })()
 
 
