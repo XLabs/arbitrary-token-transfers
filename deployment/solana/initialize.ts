9a21df945b76f95b09ce2fba14e095ec10e7e75a
scnale <sebinale@gmail.com>
2025-04-03 19:01:29 -0300

    deployment: restore line

--- deployment/solana/initialize.ts
+++ deployment/solana/initialize.ts
@@ -24,6 +24,7 @@ const initializeSolanaTbr: SolanaScriptCb = async function (
   }
 
   const deployerPubkey = new PublicKey(await signer.getAddress());
+
   const initializeIxs = await tbr.initialize(
     deployerPubkey,
     {
