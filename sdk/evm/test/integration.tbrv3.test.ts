b3e3ffdbda7877914b3e63020f9ce0b84f8335a3
Sebastián Claudio Nale <sebinale@gmail.com>
2025-05-05 13:53:39 -0300

    sdk: a few fixes to the EVM integration tests

--- sdk/evm/test/integration.tbrv3.test.ts
+++ sdk/evm/test/integration.tbrv3.test.ts
@@ -25,8 +25,8 @@ const peers = [
 describe('TbrV3 SDK Integration test', () => {
 
   before(async () => {
-    expect(rpc).to.not.be.undefined;
-    expect(ownerPk).to.not.be.undefined;
+    expect(rpc, "RPC is undefined").to.not.be.undefined;
+    expect(ownerPk, "Owner key is undefined").to.not.be.undefined;
 
     const address = new EvmAddress(config.TbrV3Proxies[0].address);
     const provider = new ethers.JsonRpcProvider(rpc, undefined, {staticNetwork: true});
